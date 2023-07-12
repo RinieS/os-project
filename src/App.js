b0da74535d34d832e87aa9939d32c32f00c63eed
Irina Skachedub <iskachedub1@gmail.com>
2023-07-12 11:33:42 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/app.css";
 import {
-  MyChat,
   Home,
   NotFound,
   UpdateProfile,
@@ -44,7 +43,6 @@ function App() {
                   <Route path="/search" element={<Search />} />
                   <Route path="/UpdateProfile" element={<UpdateProfile />} />
                   <Route path="/About" element={<About />} />
-                  <Route path="/chat" element={<MyChat/>} />
                 </Routes>
               </ScrollToTop>
             </Router>
